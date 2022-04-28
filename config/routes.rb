34b852b9fec11993af1e1be8160008e09c9dcbf1
yossu-y <yangyuub@gmail.com>
2022-04-28 03:47:43 +0000

    [add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'articles/index'
+  get 'articles/new'
+  get 'articles/show'
+  get 'articles/edit'
   devise_for :users
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
