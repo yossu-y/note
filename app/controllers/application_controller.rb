7547a5b8efa94f8f2a01a61a3e3dc727f598dce0
yossu-y <yangyuub@gmail.com>
2022-04-29 02:26:36 +0000

    [update]header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
    private
 
   def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    users_path
   end
 
   def after_sign_out_path_for(resource)
