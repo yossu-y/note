eac2ed45d1b7417f8daaed80ef757780aaf50240
yossu-y <yangyuub@gmail.com>
2022-04-29 04:58:13 +0000

    [update]view_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
    private
 
   def after_sign_in_path_for(resource)
-    users_path
+    articles_path
   end
 
   def after_sign_out_path_for(resource)
