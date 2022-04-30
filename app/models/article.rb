b6f07fff45e02b13dec7eca57b42881284fb00b7
yossu-y <yangyuub@gmail.com>
2022-04-30 09:21:28 +0000

    [update]article_index

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  
+
   has_one_attached :image
 
   belongs_to :user, optional: true
@@ -22,6 +22,14 @@ class Article < ApplicationRecord
     else
       @article = Article.all
     end
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
   end
 
   def favorited_by?(user)
